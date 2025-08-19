31fa827b5498835b59931cf0e8aec0577608e66c
Enzoc2 <enzopcunha2710@gmail.com>
2025-08-19 12:07:54 -0300

    criação dos formulários

--- website/forms.py
+++ website/forms.py
@@ -1,3 +1,9 @@
-from flask_wtf import FlaskForm 
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from .database import execute_sql
+from wtforms.validators import DataRequired, Length
+
+class PseudoLoginForm(FlaskForm):
+	id_cliente = StringField('ID do Usuário', validators=[DataRequired(), Length(min=1, max=10)])
+	cpf = StringField('CPF', validators=[DataRequired(), Length(min=1, max=14)])
+	submit = SubmitField('Entrar')
+   
--- website/routes.py
+++ website/routes.py
@@ -1,8 +1,26 @@
-from flask import Blueprint, render_template, redirect, url_for
+
+from flask import Blueprint, render_template, redirect, url_for, request, session, flash
 from website.database import execute_sql
+from website.forms import PseudoLoginForm
 
 routes = Blueprint('routes', __name__)  
 
-@routes.route('/')
+@routes.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    form = PseudoLoginForm()
+    if form.validate_on_submit():
+        # Aqui você pode validar o usuário no banco, se quiser
+        session['id_cliente'] = form.id_cliente.data
+        session['cpf'] = form.cpf.data
+        return redirect(url_for('routes.menu'))
+    return render_template('home.html', form=form)
+
+@routes.route('/menu')
+def menu():
+    id_cliente = session.get('id_cliente')
+    if not id_cliente:
+        flash('Faça login para acessar o menu.')
+        return redirect(url_for('routes.home'))
+    # Exemplo: buscar informações do usuário
+    # usuario = execute_sql('SELECT * FROM Usuario WHERE ID_Cliente = %s', (id_cliente,), fetch_one=True)
+    return render_template('menu.html', id_cliente=id_cliente)
